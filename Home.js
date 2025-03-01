e0aafa64e9679105a206a77c52cf9076eb993895
Sithi-Vignesh <sithivignesh11@gmail.com>
2025-03-01 15:46:23 +0530

    gamez page

--- Home.js
+++ Home.js
@@ -1,3 +1,5 @@
+document.querySelector('.hamburger-icon').style.cursor = 'pointer';
+
 document.querySelector('.hamburger-icon').addEventListener('click', function() {
     const menu = document.querySelector('.hamburger');
     if (menu.style.display === 'none' || menu.style.display === '') {
