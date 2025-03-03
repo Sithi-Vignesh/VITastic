cbce8247a6e2701dda165a5ed048ea434437d02a
Sithi-Vignesh <sithivignesh11@gmail.com>
2025-03-03 20:05:33 +0530

    carousel

--- Home.js
+++ Home.js
@@ -8,3 +8,44 @@ document.querySelector('.hamburger-icon').addEventListener('click', function() {
         menu.style.display = 'none';
     }
 });
+
+const slider = document.querySelector('.slider');
+const slides = slider.querySelectorAll('img');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+let currentIndex = 0;
+let autoSlideInterval;
+
+function showSlide(index) {
+    slider.scrollTo({
+        left: slider.clientWidth * index,
+        behavior: 'smooth'
+    });
+    currentIndex = index;
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+}
+
+prevButton.addEventListener('click', prevSlide);
+nextButton.addEventListener('click', nextSlide);
+
+function startAutoSlide() {
+    autoSlideInterval = setInterval(nextSlide, 3000);
+}
+
+function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+}
+
+slider.addEventListener('mouseenter', stopAutoSlide);
+slider.addEventListener('mouseleave', startAutoSlide);
+
+startAutoSlide();
