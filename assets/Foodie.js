d173caa7e57da7ae64e9c91d17638d67214c2729
Sithi-Vignesh <sithivignesh11@gmail.com>
2025-04-02 00:58:34 +0530

    chatbot - Cylo

--- assets/Foodie.js
+++ assets/Foodie.js
@@ -294,3 +294,6 @@ document.addEventListener('DOMContentLoaded', () => {
       }, index * 100); 
   });
 });
+
+
+(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="xkpsr3lwPLeesWZcgY_Mp";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
